72e03767f192bc01d16c48ab3540ca5f76595bc7
LauraP <166152671+LauraPeckyno@users.noreply.github.com>
2024-08-14 14:38:14 -0400

    duplicated the attack function from the enemy side to the player side

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 class Player {
     constructor(name, hull, firepower, accuracy) {
         this.name = name;
@@ -6,7 +5,18 @@ class Player {
         this.firepower = firepower;
         this.accuracy = accuracy;
     }
+    attack() {
+        const roll = Math.random();
+        if (roll <= this.accuracy) {
+            console.log(`${this.name} hits ${Enemy.name}!`); // is your roll less than your accuracy? 
+            const damage = Math.floor(Math.random() * this.firepower) + 1;
+            Enemy.hull -= damage;
+            console.log(`${this.name} deals ${damage} damage to ${Enemy.name}!`);  // damage
+        } else {
+            console.log(`${this.name} misses ${Enemy.name}!`);  // if no, then miss
+        }
     };
+}  /// duplicating the attack from the enemy side to the player side
 
 class Enemy {
     constructor(name, hull, firepower, accuracy) {
@@ -19,32 +29,32 @@ class Enemy {
         // random roll to see if you hit
         const roll = Math.random();
         if (roll <= this.accuracy) {
-            console.log(`${this.name} hits ${Player.name}!`); // is your roll less than your accuracy? 
+            console.log(`${this.name} damages ${Player.name}!`); // is your roll less than your accuracy? 
             // if yes, then roll for damage
-            const damage = Math.floor(Math.random() * this.firepower) +1;  // random number decimal times the firepower, then rounded. add 1 to ensure it's the minimum amount of damage to hit.
+            const damage = Math.floor(Math.random() * this.firepower) + 1;  // random number decimal times the firepower, then rounded. add 1 to ensure it's the minimum amount of damage to hit.
             Player.hull -= damage;
-            console.log(`${this.name} deals ${damage} damage to ${Player.name}!`);  // damage logged
+            console.log(`${this.name} hits ${Player.name} for ${damage}!`);  // damage logged
         } else {
             console.log(`${this.name} misses ${Player.name}!`);  // if no, then miss
         }
     };
 }
 
-const laura = new Player("Laura", 20, 5, 0.7);
+const laura = new Player("laura", 20, 5, 0.7);
 const baddie = new Enemy("baddie", 4, 3, 0.6);
 
 
 
 /// from the assignment brief ---function to determine hit accuracy
-// if (Math.random() < Enemy[0].accuracy) {  
+// if (Math.random() < Enemy[0].accuracy) {
 // 	console.log('You have been hit!');
 // };
 // random roll a number for your attack against the current enemy
 // numbers <= accuracy are a hit?
-// damage = 
+// damage =
 // determine the damage, then determine how much hull is left
 
-// three rolls?  one to see if you hit and one to see the damage (roll times accuracy), then 
+// three rolls?  one to see if you hit and one to see the damage (roll times accuracy), then
 
 // /// our name = USS Assembly
 /// there are 6 enemy ships
@@ -66,7 +76,7 @@ const baddie = new Enemy("baddie", 4, 3, 0.6);
 // You could be battling six alien ships each with unique values.
 
 // The aliens attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. \
-// You have the initiative and get to attack first. You can only attack the aliens in order. 
+// You have the initiative and get to attack first. You can only attack the aliens in order.
 // After you have destroyed a ship, you have the option to make a hasty retreat. Retreat = defeat
 
 // A game round would look like this:
@@ -78,9 +88,3 @@ const baddie = new Enemy("baddie", 4, 3, 0.6);
 // If you retreat, the game is over, perhaps leaving the game open for further developments or options
 // You win the game if you destroy all of the aliens
 // You lose the game if you are destroyed
-
-//from the biref////
-    // roll(mod = 0) {
-    //     const result = Math.floor(Math.random() * 20) + 1 + mod;
-    //     console.log(`${this.name} rolled a ${result}.`)
-    // }
